340227ff15acfe0b118af3611972b318961b93bd
yiyuezhuo <616271835@163.com>
2017-06-25 10:26:32 +0800

    add some experiments

---
+++ test5.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 24 09:03:03 2017
+
+@author: yiyuezhuo
+"""
+
+import pymc3 as pm
+import theano.tensor as T
+
+pm.Metropolis
+
+import edward as ed
+
+ed.models.Mixture
